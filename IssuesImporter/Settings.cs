c94d50f430bf4fe2175ba5ed8da5ef5231a10724
Steve Bohlen <sbohlen@gmail.com>
2014-12-23 07:58:42 -0500

    introduce Settings class

---
+++ IssuesImporter/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssuesImporter
+{
+    public class Settings
+    {
+        public string GitHubApiAccessToken { get; private set; }
+        public string GitHubApiProductHeaderValue { get; private set; }
+        public string GitHubRepositoryOwner { get; private set; }
+        public string GitHubRepositoryName { get; private set; }
+        public string GoogleCodeProjectName { get; private set; }
+        public string GoogleCodeProjectUrl { get; private set; }
+        public string GoogleCodeIssueUrlStringFormatTemplate { get; private set; }
+        public int GitHubApiThrottleOnCreateInvocationCount { get; private set; }
+        public int GitHubApiThrottleOnCreatePauseDurationMilliseconds { get; private set; }
+    }
+}
