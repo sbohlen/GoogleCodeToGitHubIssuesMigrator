8ce9af2fee3d55288da2f5fbc321c7efbab157a2
Steve Bohlen <sbohlen@gmail.com>
2014-12-23 06:15:24 -0500

    functioning import-update flow

--- IssuesImporter.Test/WhenWritingGithubIssues.cs
+++ IssuesImporter.Test/WhenWritingGithubIssues.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Octokit;
@@ -13,6 +16,9 @@ namespace IssuesImporter.Test
     {
         private GoogleIssuesDataFileReader _reader;
         private const string InputDataFile = @"TestData\TestInputDataFile.csv";
+        private const string AccessToken = "03f37ffab000ed46dfa912a554f03d2cc8bf2d67";
+        private const string GitHubRepositoryOwner = "NDbUnit";
+        private const string GitHubRepositoryName = "NDbUnit";
 
         private GitHubClient _client;
         private Credentials _basicAuth;
@@ -21,18 +27,17 @@ namespace IssuesImporter.Test
         [Test]
         public async void CanAddIssueToGitHub()
         {
-            _client = new GitHubClient(new ProductHeaderValue("sbohlen-test-2"));
-            _basicAuth = new Credentials("af152b92524012fbd5c79230fac6b4570f6451e1");
+            _client = new GitHubClient(new ProductHeaderValue("google%20code%20to%20github%20issue%20migrator"));
+            _basicAuth = new Credentials(AccessToken);
 
             _client.Credentials = _basicAuth;
             _issuesClient = _client.Issue;
 
-
             _reader = new GoogleIssuesDataFileReader(InputDataFile);
 
-            var googleIssues = _reader.GetIssues();
+            var googleIssues = _reader.GetIssues().ToList();
 
-            var gitHubIssues = new List<NewIssue>();
+            var gitHubIssuesToCreate = new List<NewIssue>();
 
             //first step, create the issues...
             foreach (var googleIssue in googleIssues)
@@ -40,17 +45,88 @@ namespace IssuesImporter.Test
                 var newIssue = new NewIssue(googleIssue.Summary);
                 newIssue.Body =
                     string.Format(
-                    "This issue ported from the deprecated GoogleCode site (https://code.google.com/p/ndbunit)." +
-                    "\n\nSee https://code.google.com/p/ndbunit/issues/detail?id={0} for more history and details for this issue.",
+                    "This issue ported from the deprecated GoogleCode NDbUnit site (https://code.google.com/p/ndbunit)." +
+                    "\n\nSee https://code.google.com/p/ndbunit/issues/detail?id={0} for more history and details for this specific issue.",
                     googleIssue.Id);
 
-                gitHubIssues.Add(newIssue);
+                gitHubIssuesToCreate.Add(newIssue);
             }
 
+            var gitHubIssuesCreated = new List<Issue>();
+
+            var issueCounter = 1.0d;
+
+
             //next step, add them to the github instance ...
-            foreach (var gitHubIssue in gitHubIssues)
+            foreach (var gitHubIssue in gitHubIssuesToCreate)
             {
-                await _issuesClient.Create("NDbUnit", "test-issues-import", gitHubIssue);
+
+                if (Math.Abs(issueCounter % 20.0) < 0.1)
+                {
+                    Debug.WriteLine("Sleeping for 20s...");
+                    Thread.Sleep(70000);
+                }
+
+                Issue issue = await _issuesClient.Create(GitHubRepositoryOwner, GitHubRepositoryName, gitHubIssue);
+
+                Debug.WriteLine("Added Issue #{0} to GitHub.", issue.Number);
+
+                gitHubIssuesCreated.Add(issue);
+
+                issueCounter++;
+            }
+
+            //final step, update each issue to reflect correct status, etc.
+            foreach (var gitHubIssue in gitHubIssuesCreated)
+            {
+                var gitHubIssueToUpdate = await _issuesClient.Get(GitHubRepositoryOwner, GitHubRepositoryName, gitHubIssue.Number);
+
+                if (null == gitHubIssueToUpdate)
+                {
+                    Debugger.Break();
+                }
+
+                if (!googleIssues.Any(googleIssue => googleIssue.Id == gitHubIssue.Number))
+                {
+                    Debugger.Break();
+                }
+
+
+                var correspondingGoogleIssue = googleIssues.Single(s => s.Id == gitHubIssue.Number);
+
+                Debug.WriteLine("Processing Issue #{0}", correspondingGoogleIssue.Id);
+
+                var issueUpdate = new IssueUpdate();
+                var newLabels = new List<string>();
+
+                if (!correspondingGoogleIssue.IsOpen)
+                {
+                    issueUpdate.State = ItemState.Closed;
+                }
+
+                if (correspondingGoogleIssue.IsDefect)
+                {
+                    newLabels.Add("bug");
+                }
+
+                if (correspondingGoogleIssue.IsEnhancement)
+                {
+                    newLabels.Add("enhancement");
+                }
+
+                if (correspondingGoogleIssue.IsWontFix)
+                {
+                    newLabels.Add("wontfix");
+                }
+
+                if (correspondingGoogleIssue.IsInvalid)
+                {
+                    newLabels.Add("invalid");
+                }
+
+                issueUpdate.Labels = newLabels;
+
+                await _issuesClient.Update(GitHubRepositoryOwner, GitHubRepositoryName, gitHubIssue.Number, issueUpdate);
             }
         }
     }
--- IssuesImporter/GoogleIssue.cs
+++ IssuesImporter/GoogleIssue.cs
@@ -12,5 +12,34 @@ namespace IssuesImporter
         public string Owner { get; set; }
         public string Summary { get; set; }
         public IEnumerable<string> Labels { get; set; }
+
+        public bool IsOpen
+        {
+            get
+            {
+                var candidate = Status.ToUpper();
+                return candidate == "NEW" || candidate == "ACCEPTED" || candidate == "STARTED";
+            }
+        }
+
+        public bool IsDefect
+        {
+            get { return IssueType.ToUpper() == "DEFECT"; }
+        }
+
+        public bool IsEnhancement
+        {
+            get { return IssueType.ToUpper() == "ENHANCEMENT"; }
+        }
+
+        public bool IsWontFix
+        {
+            get { return Status.ToUpper() == "WONTFIX"; }
+        } 
+        
+        public bool IsInvalid
+        {
+            get { return Status.ToUpper() == "INVALID"; }
+        }
     }
 }
